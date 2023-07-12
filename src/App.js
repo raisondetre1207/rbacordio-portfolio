9d9f4740f2fd1aa664337991f3ee0a5d49c04aca
rbacordio <rbacordio@fullscale.io>
2023-07-12 22:54:45 +0800

    Use the createRoot() method from react-dom/client

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 const App = () => {
-  return <h1>Hello React</h1>;
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <h1>Sample Counter</h1>
+      <h3>This is a sample react app</h3>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(prev => prev + 1)}>+</button>
+      <button onClick={() => setCounter(prev => prev-1)}>-</button>
+    </div>);
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.scss";
 
-const el = document.getElementById("app");
+const root = ReactDOM.createRoot(document.getElementById("app"));
 
-ReactDOM.render(<App />, el);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>);
