fb768daa156374eadde29bea23036845a3398307
rbacordio <rbacordio@fullscale.io>
2023-07-13 21:38:20 +0800

    Remove bullet points in navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import NavBar from './components/NavBar.jsx'
 const App = () => {
   const [counter, setCounter] = useState(0);
   return (
     <div>
+      <NavBar/>
       <h1>Sample Counter</h1>
       <h3>This is a sample react app</h3>
       <h1>{counter}</h1>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NavBar(){
+    return (
+        <ul className="navbar">
+            <li>Home</li>
+            <li>Projects</li>
+            <li>About me</li>
+            <li>Contact me</li>
+        </ul>
+    );
+}
+
+export default NavBar;
